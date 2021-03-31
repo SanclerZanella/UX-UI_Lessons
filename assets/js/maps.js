9630f3d219f92335fa13f29c81d4f7f27d3289ea
Sancler Zanella <sanclerzjj@gmail.com>
2021-03-31 19:12:01 +0000

    Hide some files

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,38 +1,33 @@
- var mapsAPIKey = config.googleMapKey;
- let mapRender = document.getElementById("gMaps");
-
- mapRender.setAttribute("src", `https://maps.googleapis.com/maps/api/js?key=${mapsAPIKey}&callback=initMap`);
- 
- function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: 46.619261,
-                    lng: -33.134766
-                }
-            });
-
-            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-
-            var locations = [{
-                lat: 40.785091,
-                lng: -73.968285
-            }, {
-                lat: 41.084045,
-                lng: -73.874256
-            }, {
-                lat: 40.754932,
-                lng: -73.984016
-            }];
-
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
-
-            var markerCluster = new MarkerClusterer(map, markers, {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
         }
+    });
+
+    var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+    var locations = [{
+        lat: 40.785091,
+        lng: -73.968285
+    }, {
+        lat: 41.084045,
+        lng: -73.874256
+    }, {
+        lat: 40.754932,
+        lng: -73.984016
+    }];
+
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+}
